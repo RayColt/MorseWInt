90a5b68575832f440b0be222f765877e8a898cfd
raycolt <ray_colt@outlook.com>
2026-04-09 15:02:39 +0200

    upd

--- MorseWInt/MorseWav.cpp
+++ MorseWInt/MorseWav.cpp
@@ -184,7 +184,7 @@ void MorseWav::Tones(int silence)
 * Define dit, dah and space.
 *
 * The rules of 1/3/7 and 1/2/4 timing conventions
-* For Mobile microphones etc I made:
+* Mobile (micro)phones will compres morse tones, better do it yourself:
 * symbol space is one silence
 * letter space is two silences
 * word space is four silences
